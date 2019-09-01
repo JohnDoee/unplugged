c4678d4176b0ad5cf88ff6e655fadf603ab0954f
Anders Jensen <johndoee@tidalstream.org>
2019-09-01 09:35:56 +0200

    fixed missing manifest

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'requirements
 
 setup(
     name="unplugged",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
 
     install_requires=requirements,
