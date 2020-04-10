755227a5319ea443c3d8f1356380981430c80419
Anders Jensen <jd@tridentstream.org>
2020-04-10 19:57:18 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'requirements
 
 setup(
     name="unplugged",
-    version="0.1.8",
+    version="0.1.9",
     packages=find_packages(),
     package_data={
         '': [
